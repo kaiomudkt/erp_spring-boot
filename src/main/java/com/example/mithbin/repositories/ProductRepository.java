3dd4090a164de1249060a95e0451cd87197d9cb8
Kaio Lino <kaiomudkt@gmail.com>
2023-08-05 17:59:04 -0400

    feat: criando crud

---
+++ src/main/java/com/example/mithbin/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.mithbin.repositories;
+
+import com.example.mithbin.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
