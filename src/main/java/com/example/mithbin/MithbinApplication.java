f2627d149a36e7b19e0c49f8d2e57b00c20fee56
Kaio Lino <kaiomudkt@gmail.com>
2023-08-05 09:47:55 -0400

    inicio projeto springboot

---
+++ src/main/java/com/example/mithbin/MithbinApplication.java
@@ -0,0 +1,13 @@
+package com.example.mithbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MithbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MithbinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mithbin/MithbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mithbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MithbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
