b37730aa75d8be66bae68e96650e59ad5c0ba485
Kaio Lino <kaiomudkt@gmail.com>
2023-08-06 17:34:27 -0400

    implementando links response json hateons

--- src/main/java/com/example/mithbin/controllers/ProductController.java
+++ src/main/java/com/example/mithbin/controllers/ProductController.java
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -34,7 +34,18 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProduct() {
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productsList = productRepository.findAll();
+        if(!productsList.isEmpty()) {
+            for(ProductModel product : productsList) {
+                UUID id = product.getIdProduct();
+                product.add(
+                        linkTo(
+                                methodOn(ProductController.class).getOneProduct(id)
+                        ).withSelfRel()
+                );
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
 
     @GetMapping("/products/{id}")
@@ -43,6 +54,9 @@ public class ProductController {
         if(product0.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
         }
+        product0.get().add(
+                linkTo(methodOn(ProductController.class).getAllProduct()).withRel("Lista de produtos")
+        );
         return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
 
